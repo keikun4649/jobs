53866ceabd6098bfebe089e15803fa0386a31322
keikun4649 <kei.hrose@gmail.com>
2025-05-26 16:42:53 +0900

    ルートを修正

--- routes/web.php
+++ routes/web.php
@@ -4,16 +4,11 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JobController;
 use App\Http\Controllers\Admin\JobAdminController;
 
-// 一般ユーザー用
-Route::get('/', function () {
-    return redirect()->route('jobs.top');
-});
-
-Route::get('/jobs', [JobController::class, 'top'])->name('jobs.top');
-Route::get('/jobs/{category}', [JobController::class, 'index'])->name('jobs.index');
-Route::get('/jobs/{category}/{job}', [JobController::class, 'show'])->name('jobs.show');
+Route::get('/', [JobController::class, 'top'])->name('jobs.top');
+Route::get('/{category}', [JobController::class, 'index'])->name('jobs.index');
+Route::get('/{category}/{job}', [JobController::class, 'show'])->name('jobs.show');
 
 // 管理者用
 Route::prefix('admin')->name('admin.')->group(function () {
-    Route::resource('jobs', JobAdminController::class);
+    Route::resource('/', JobAdminController::class);
 });
